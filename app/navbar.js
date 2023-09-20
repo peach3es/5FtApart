c63ad0980331c51b44d7307cd21468719d25d64f
Peizhe Tian <peizhe2001@hotmail.com>
2023-09-20 14:21:40 -0400

    Edit navbar, but still problem with page linking

--- app/navbar.js
+++ app/navbar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./navbar-footer.css";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav class="navbar navbar-dark navbar-expand-lg fixed-top">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
-          Navbar
+          Home Hub
         </a>
         <button
           class="navbar-toggler"
@@ -22,14 +23,51 @@ const Navbar = () => {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
+              <Link class="nav-link" aria-current="page" href="/search">
+                Search
+              </Link>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">
-                Link
+              <Link class="nav-link" href="/buy">
+                Buy
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" href="/sell">
+                Sell
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" href="/my-broker">
+                My Broker
+              </Link>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown"
+                role="button"
+                aria-expanded="false"
+                data-bs-offset="10,20"
+              >
+                Tools
               </a>
+              <ul class="dropdown-menu">
+                <li>
+                  <Link class="dropdown-item" href="/calculator">
+                    Calculator
+                  </Link>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Another section
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Click for Kanye nudes
+                  </a>
+                </li>
+              </ul>
             </li>
           </ul>
         </div>
