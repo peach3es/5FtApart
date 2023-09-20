86e5ed07319d98228797bb6254ea4a24d9d324cb
peaches <peizhe2001@hotmail.com>
2023-09-19 22:42:38 -0400

    change color in footer text

--- app/footer.js
+++ app/footer.js
@@ -4,31 +4,31 @@ import "./navbar-footer.css";
 const Footer = () => {
   return (
     <footer class="d-flex justify-content-between align-items-center p-3 mt-4 border-top sticky-bottom footer">
-      <p class="col-md-4 mb-0 text-body-secondary">© 2023 HomeHub</p>
+      <p class="col-md-4 mb-0 text">© 2023 HomeHub</p>
 
       <ul class="nav col-md-4 justify-content-end">
         <li class="nav-item">
-          <a href="#" class="nav-link px-2 text-body-secondary">
+          <a href="#" class="nav-link px-2 text">
             Home
           </a>
         </li>
         <li class="nav-item">
-          <a href="#" class="nav-link px-2 text-body-secondary">
+          <a href="#" class="nav-link px-2 text">
             Features
           </a>
         </li>
         <li class="nav-item">
-          <a href="#" class="nav-link px-2 text-body-secondary">
+          <a href="#" class="nav-link px-2 text">
             Pricing
           </a>
         </li>
         <li class="nav-item">
-          <a href="#" class="nav-link px-2 text-body-secondary">
+          <a href="#" class="nav-link px-2 text">
             FAQs
           </a>
         </li>
         <li class="nav-item">
-          <a href="#" class="nav-link px-2 text-body-secondary">
+          <a href="#" class="nav-link px-2 text">
             About
           </a>
         </li>
--- app/page.js
+++ app/page.js
@@ -2,42 +2,12 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import "./navbar-footer.css";
 import Footer from "./footer";
+import Navbar from "./navbar";
 
 export default function Home() {
   return (
     <div className="main-page">
-      <nav class="navbar navbar-dark navbar-expand-md fixed-top">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            Navbar
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Link
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Navbar />
       <main className={styles.main}></main>
       <Footer />
     </div>
