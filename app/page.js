2557470361806d7d09ba4cff9ba74018c4492a07
Peizhe Tian <peizhe2001@hotmail.com>
2023-09-22 15:50:39 -0400

    Add carousel but still not working

--- app/page.js
+++ app/page.js
@@ -2,11 +2,13 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Footer from "../components/footer/footer";
 import Navbar from "../components/navbar/navbar";
+import Carousel from "../components/gallery/carousel";
 
 export default function Home() {
   return (
     <div className="main-page">
       <Navbar />
+      <Carousel />
       <main className={styles.main}></main>
       <Footer />
     </div>
---
+++ components/gallery/carousel.js
@@ -0,0 +1,69 @@
+import React from "react";
+import "styles/carousel.css";
+// import pic1 from "..../pictures/homepage/pic1.jpg";
+
+const Carousel = () => {
+  return (
+    <div className="container">
+      <div id="carouselExampleIndicators" class="carousel slide">
+        <div class="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="0"
+            class="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img
+              src="pictures/homepage/pic1.jpg"
+              class="d-block w-100"
+              alt="..."
+            ></img>
+          </div>
+          <div class="carousel-item">
+            <img src="..." class="d-block w-100" alt="..."></img>
+          </div>
+          <div class="carousel-item">
+            <img src="..." class="d-block w-100" alt="..."></img>
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
