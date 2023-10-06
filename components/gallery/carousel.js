daf02cbb691b596c7da866af85f45b8c7d78b59f
peaches <peizhe2001@hotmail.com>
2023-10-06 00:55:42 -0400

    change class to className in carousel

--- components/gallery/carousel.js
+++ components/gallery/carousel.js
@@ -6,15 +6,15 @@ const Carousel = () => {
   return (
     <div
       id="carouselExampleIndicators"
-      class="carousel slide carousel-fade"
+      className="carousel slide carousel-fade"
       data-bs-ride="carousel"
     >
-      <div class="carousel-indicators">
+      <div className="carousel-indicators">
         <button
           type="button"
           data-bs-target="#carouselExampleIndicators"
           data-bs-slide-to="0"
-          class="active"
+          className="active"
           aria-current="true"
           aria-label="Slide 1"
         ></button>
@@ -31,8 +31,8 @@ const Carousel = () => {
           aria-label="Slide 3"
         ></button>
       </div>
-      <div class="carousel-inner">
-        <div class="carousel-item active" style={{ height: "50vh" }}>
+      <div className="carousel-inner">
+        <div className="carousel-item active" style={{ height: "50vh" }}>
           <Image
             src="/pictures/homepage/pic1.jpg"
             alt="Picture of the author"
@@ -43,7 +43,7 @@ const Carousel = () => {
             style={{ width: "100%", height: "auto" }}
           />
         </div>
-        <div class="carousel-item">
+        <div className="carousel-item">
           <Image
             src="/pictures/homepage/pic2.jpg"
             alt="Picture of the author"
@@ -52,27 +52,27 @@ const Carousel = () => {
             sizes="100vw"
           />
         </div>
-        <div class="carousel-item">
-          <img src="..." class="d-block w-100" alt="..." />
+        <div className="carousel-item">
+          <img src="..." className="d-block w-100" alt="..." />
         </div>
       </div>
       <button
-        class="carousel-control-prev"
+        className="carousel-control-prev"
         type="button"
         data-bs-target="#carouselExampleIndicators"
         data-bs-slide="prev"
       >
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
       </button>
       <button
-        class="carousel-control-next"
+        className="carousel-control-next"
         type="button"
         data-bs-target="#carouselExampleIndicators"
         data-bs-slide="next"
       >
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
       </button>
     </div>
   );
