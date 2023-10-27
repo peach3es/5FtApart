fcf06fb2d060b01aff9b8b782c0300c42ee04841
Peizhe Tian <peizhe2001@hotmail.com>
2023-10-27 16:55:00 -0400

    change some login stylings

--- components/Login/form.tsx
+++ components/Login/form.tsx
@@ -24,7 +24,7 @@ function LogInForm() {
 
   return (
     <div className="flex flex-row gap-5 my-5 justify-center place-items-center w-full">
-      <div className="login-container w-1/2 justify-center place-items-center flex bg-slate-300 h-full ml-10 rounded-xl">
+      <div className="login-container w-1/2 justify-center place-items-center flex bg-slate-300 h-full ml-5 rounded-xl">
         <form
           className="bg-[#eeeeee] flex flex-col justify-center rounded-xl w-1/2"
           id="Login"
@@ -120,102 +120,8 @@ function LogInForm() {
             </Tab>
           </Tabs>
         </form>
-        {/* // Signup Form
-          <div className="bg-[#eeeeee]" id="SignUp">
-            <div className="col-md-6">
-              <label htmlFor="inputEmail4" className="form-label">
-                Email
-              </label>
-              <input
-                type="email"
-                className="form-control"
-                id="inputEmail4"
-                placeholder="johndoe@gmail.com"
-              />
-            </div>
-            <div className="col-md-6">
-              <label htmlFor="inputPassword4" className="form-label">
-                Password
-              </label>
-              <input
-                type="password"
-                className="form-control"
-                id="inputPassword4"
-                placeholder="********"
-              />
-            </div>
-            <div className="col-md-6">
-              <label htmlFor="inputAddress" className="form-label">
-                Phone Number
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="inputAddress"
-                placeholder="514-420-6969"
-              />
-            </div>
-            <div className="col-md-6">
-              <label htmlFor="inputAddress2" className="form-label">
-                Broker ID
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="inputAddress2"
-                placeholder="32445332"
-              />
-            </div>
-            <div className="col-md-6">
-              <label htmlFor="inputCity" className="form-label">
-                City
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="inputCity"
-                placeholder="GainCity"
-              />
-            </div>
-
-            <div className="col-md-6">
-              <label htmlFor="inputZip" className="form-label">
-                Size?
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="inputZip"
-                placeholder="big."
-              />
-            </div>
-
-            <div className="btn">
-              <button type="submit" className="btn btn-primary">
-                Turn up
-              </button>
-            </div>
-
-            <div>
-              <div className="form-check form-switch">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  role="switch"
-                  id="flexSwitchCheckDefault"
-                />
-                <label
-                  className="form-check-label"
-                  htmlFor="flexSwitchCheckDefault"
-                >
-                  Broker?
-                </label>
-              </div>
-            </div>
-          </div>
-      </div> */}
       </div>
-      <div className="w-1/2 mr-10">
+      <div className="w-1/2 mr-5">
         <Image
           src={randomImage}
           alt="5ftapart"
@@ -224,7 +130,7 @@ function LogInForm() {
           sizes="100vw"
           style={{
             objectFit: "cover",
-            height: "70vh",
+            height: "90vh",
           }}
         />
       </div>
