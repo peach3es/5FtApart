04201265909bfbb173ffb47071e77c9f9dcb4273
Thomas Kamil <thomasbrochet1@gmail.com>
2023-11-04 14:16:00 -0400

    update about us

--- app/(page)/about/page.tsx
+++ app/(page)/about/page.tsx
@@ -1,10 +1,66 @@
-import { Card,CardBody,CardHeader, Table,TableBody,TableColumn,TableHeader,TableCell,TableRow, Button } from "@nextui-org/react";
+"use client"
 
+import { Card,CardBody,CardHeader, Table,TableBody,TableColumn,TableHeader,TableCell,TableRow,getKeyValue, Button } from "@nextui-org/react";
+const rows = [
+  {
+    key: "1",
+    name: "Thomas Kamil Brochet",
+    role: "Front-End",
+    id: "40121143",
+  },
+  {
+    key: "2",
+    name: "Charles Atanacio",
+    role: "BackEnd",
+    id: "40176314",
+  },
+  {
+    key: "3",
+    name: "Alexander Smagorinski",
+    role: "Full Stack Developer",
+    id: "40190986",
+  },
+  {
+    key: "4",
+    name: "Zhan Jun Cheung",
+    role: "Front End",
+    id: "40212301",
+  },
+  {
+    key: "5",
+    name: "Peizhe Tian",
+    role: "Front End Developer",
+    id: "40191463",
+  },
+  {
+    key: "6",
+    name: "Patrick",
+    role: "BackEnd",
+    id: "40212301",
+  }
+
+
+];
+
+const columns = [
+  {
+    key: "name",
+    label: "NAME",
+  },
+  {
+    key: "role",
+    label: "ROLE",
+  },
+  {
+    key: "id",
+    label: "Studend ID",
+  },
+];
 const HomePage = () => {
   return (
     <div className="p-10">
       <div className="max-w-4xl mx-auto">
-        <h1 className="mb-6">
+        <h1 className="mb-6 font-weight bold">
           Happy Rainbow Unicorn - Real Estate Web Application
         </h1>
 
@@ -14,63 +70,49 @@ const HomePage = () => {
           </CardHeader>
           <CardBody>
             <ul className="list-disc pl-5">
-              <li>Next.js 13: Web development framework...</li>
-              {/* Add all the technologies similarly */}
+              <li>Next.js 13: Web development framework used with React that provides Server Side Rendering</li>
+              <li>NextUI v2: CSS library built on top of tailwind to add extra stylings to website</li>
+              <li>Tailwind CSS: CSS library that helps styling the website</li>
+              <li>Tailwind Variants: CSS library extension for tailwind</li>
+              <li>ReactJS: Frontend library to facilitate creation of website</li>
+              <li>ExpressJS: Backend</li>
+              <li>Mongoose: Backend database used to store different values for users, properties, etc.</li>
+             
             </ul>
           </CardBody>
         </Card>
 
-        <Card className="mb-6">
-          <CardHeader>
-            <h2>How to Use</h2>
-          </CardHeader>
-          <CardBody>
-            <ol className="list-decimal pl-5">
-              <li>Clone this repo...</li>
-              {/* Add other steps similarly */}
-            </ol>
-          </CardBody>
-        </Card>
-
+      
         <h2 className="mb-4">Project Description</h2>
         <p>
-          {/* Add your project description here */}
+        A real estate web application is a digital platform designed to facilitate the buying, selling, and renting, of real estate properties. These applications offer a wide range of features and functionalities to streamline the entire real estate transaction process, making it more convenient and efficient for users.
         </p>
 
-        <h2 className="my-4">List of Main Use Cases</h2>
+        <h2 className="my-4">List of Main Users</h2>
         <p>
-          {/* Describe use cases here */}
+          HomeBuyers
         </p>
 
-        <h2 className="my-4">Team Members and Roles</h2>
+        <p>
+          Property Renter
+        </p>
+
+        <p>
+          Brokers
+        </p>
+
+        <h2 className="my-4" font-weight="bold">Team Members and Roles</h2>
         
-        <Table>
-      <TableHeader>
-        <TableColumn>NAME</TableColumn>
-        <TableColumn>ROLE</TableColumn>
-        <TableColumn>ID</TableColumn>
+        <Table aria-label="Example table with dynamic content">
+      <TableHeader columns={columns}>
+        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
       </TableHeader>
-      <TableBody>
-        <TableRow key="1">
-          <TableCell>Tony Reichert</TableCell>
-          <TableCell>CEO</TableCell>
-          <TableCell>Active</TableCell>
-        </TableRow>
-        <TableRow key="2">
-          <TableCell>Zoey Lang</TableCell>
-          <TableCell>Technical Lead</TableCell>
-          <TableCell>Paused</TableCell>
-        </TableRow>
-        <TableRow key="3">
-          <TableCell>Jane Fisher</TableCell>
-          <TableCell>Senior Developer</TableCell>
-          <TableCell>Active</TableCell>
-        </TableRow>
-        <TableRow key="4">
-          <TableCell>William Howard</TableCell>
-          <TableCell>Community Manager</TableCell>
-          <TableCell>Vacation</TableCell>
-        </TableRow>
+      <TableBody items={rows}>
+        {(item) => (
+          <TableRow key={item.key}>
+            {(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
+          </TableRow>
+        )}
       </TableBody>
     </Table>
 
