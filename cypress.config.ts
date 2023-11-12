40e66481f301b655bdd5daff68aba98f141480c1
Patrick Cimpean <patrick.cimpean@gmail.com>
2023-11-11 23:53:32 -0500

    minor update to cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,21 +1,20 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      
     },
+    env: {
+      baseUrl: "http://localhost:3000" // Replace with your API's base URL
+    },
+    specPattern: "cypress/e2e"
+   
+    
 
-  
-   env: {
-        baseUrl: "http://localhost:3000" // Replace with your API's base URL
-      }
+    
   },
 
   
 });
-
-
-
-
